9e82c3554b2a7da44d283a2befc6d4baedf526cc
Sameeksha200416 <sameekshavishwakarma2004@gmail.com>
2025-05-28 18:33:17 +0530

    Add backend directory

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
-
+import path from "path";
 import dotenv from "dotenv";
 import { connectDB } from "./src/lib/db.js";
 
@@ -12,6 +12,7 @@ import messageRoutes from "./src/routes/message.route.js";
 import {app,server} from "./src/lib/socket.js";
 
 const PORT = process.env.PORT || 5001;
+const _dirname = path.resolve();
 
 app.use(express.urlencoded({ limit: "10mb", extended: true }))
 app.use(express.json({ limit: "10mb" }));
@@ -27,6 +28,13 @@ app.get("/",(req,res) => {
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(_dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(_dirname, "../frontend/dist/index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log("Server is running on PORT:"+ PORT);
